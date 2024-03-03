0d941a7b9bc5b1fd5fadbaca9953e3ebe77c819c
matthosking-mydeal <95453715+matthosking-mydeal@users.noreply.github.com>
2024-03-03 19:57:08 +1100

    update outputs to use parameter group provided

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,7 @@ output "redis_security_group_id" {
 }
 
 output "parameter_group" {
-  value = aws_elasticache_parameter_group.redis_parameter_group.id
+  value = try(var.parameter_group_name, "") != "" ? var.parameter_group_name : aws_elasticache_parameter_group.redis_parameter_group.id
 }
 
 output "redis_subnet_group_name" {
